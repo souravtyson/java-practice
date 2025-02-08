e67b5e72bfbe1bf88f9dcba957b585efd6577baa
sourav.s <sourav.s@appnomic.com>
2025-02-08 19:22:10 +0530

    another puzzle

--- src/main/java/com/practice/puzzle/Puzzle31.java
+++ src/main/java/com/practice/puzzle/Puzzle31.java
@@ -1,9 +1,7 @@
 package com.practice.puzzle;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -57,5 +55,87 @@ public class Puzzle31 {
         Integer i = Arrays.stream(intArr).boxed().distinct().sorted(Comparator.reverseOrder()).skip(2).findFirst().orElse(-1);
         System.out.println(i);
 
+        System.out.println(getEvenNumbers());
+        System.out.println(getOddNumbers());
+        System.out.println(getQuestion2());
+        System.out.println(getQuestion3());
+        System.out.println(getQuestion4());
+        System.out.println(getQuestion5());
+    }
+
+    public static List<Integer> getEvenNumbers() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        return numbers.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+    }
+
+    public static List<Integer> getOddNumbers() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        return numbers.stream().filter(n -> n % 2 != 0 ).collect(Collectors.toList());
+    }
+
+    public static List<String> getQuestion2() {
+        List<String> stringList = Arrays.asList("apple", "bat", "car", "door", "egg");
+        return stringList.stream()
+                .filter(word -> word.length() >= 4)
+                .map(String::toUpperCase)
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
+    }
+
+    public static Map<Integer, List<String>> getQuestion3() {
+        List<Person> people = Arrays.asList(
+                new Person("Alice", 30),
+                new Person("Bob", 25),
+                new Person("Charlie", 30),
+                new Person("David", 25)
+        );
+
+        return people.stream()
+                .collect(Collectors.groupingBy(Person::getAge, Collectors.mapping(Person::getName,
+                Collectors.toList())));
+    }
+
+    public static Map<String, Integer> getQuestion4() {
+        List<Integer> numbers = Arrays.asList(3, 5, 9, 10, 12, 15);
+
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("sumDivBy3", numbers.stream().filter(n -> n % 3 == 0).reduce(0, (a,b) -> a + b));
+        map.put("productDivBy5", numbers.stream().filter(n -> n % 5 == 0).reduce(1, (a,b) -> a * b));
+
+        return map;
+    }
+
+    public static Map<Character, Long> getQuestion5() {
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+
+        List<String> wordsList = Arrays.asList("apple", "orange", "banana");
+
+        return wordsList.stream()
+                .flatMap(word -> word.toLowerCase()
+                        .chars()
+                        .mapToObj(c -> (char)c)
+                ).filter(k -> vowels.contains(k))
+                .collect(Collectors.groupingBy(Character::charValue, Collectors.counting()));
+
+    }
+
+
+    static class Person {
+        String name;
+        int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
     }
 }
